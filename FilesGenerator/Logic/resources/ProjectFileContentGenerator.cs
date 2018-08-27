120b8a8ce3f1adba9335e0e556b375ee4faa9dec
WinnieThePoooh <aspilnyy@gmail.com>
2018-08-27 13:51:06 +0200

    add templates for resourceSource and Genereated files

--- FilesGenerator/Logic/resources/ProjectFileContentGenerator.cs
+++ FilesGenerator/Logic/resources/ProjectFileContentGenerator.cs
@@ -6,20 +6,37 @@ namespace FilesGenerator.Logic.resources
     public class ProjectFileContentGenerator : IFileContentGenerator
     {
         private readonly string _projFileContent;
+        private readonly string _resourceFileTemplate;
+        private string _resourceFileDesignerTemplate;
 
         public ProjectFileContentGenerator()
         {
-            _projFileContent = new ResourceReader().Read("FilesGenerator.Logic.resources.ProjFileContent.txt");
+            _projFileContent = new ResourceReader().Read("FilesGenerator.Logic.resources.ProjFileTemplate.txt");
+            _resourceFileTemplate = new ResourceReader().Read("FilesGenerator.Logic.resources.ResourceFileTemplate.txt");
+            _resourceFileDesignerTemplate = new ResourceReader().Read("FilesGenerator.Logic.resources.ResourceFileDesignerTemplate.txt");
         }
 
         public IEnumerable<GeneratedFile> Generate(string classSuffix)
         {
-            var content = _projFileContent
+            var resourceFileName = "res" + classSuffix;
+            var projFileContent = _projFileContent
                 .Replace("$projectId", Guid.NewGuid().ToString())
                 .Replace("$rootNamespace", "Namespace" + classSuffix)
                 .Replace("$assemblyName", "AssemblyName" + classSuffix)
-                .Replace("$resourceFileName", "Resource" + classSuffix);
-            return new [] { new GeneratedFile("proj" + classSuffix + ".csproj", content)};
+                .Replace("$ProjectReference", string.Empty)
+                .Replace("$resourceFileName", resourceFileName);
+            var resourceItemName = "resource" + classSuffix + "ItemName";
+            var resourceFileContent = _resourceFileTemplate
+                .Replace("$resourceItemName", resourceItemName)
+                .Replace("$resourceItemValue", "resource" + classSuffix + "Value");
+            var resourceFileDesignerContent = _resourceFileDesignerTemplate
+                .Replace("$resourceItemName", resourceItemName);
+            return new []
+            {
+                new GeneratedFile("proj" + classSuffix + ".csproj", projFileContent),
+                new GeneratedFile(resourceFileName + ".resx", resourceFileContent),
+                new GeneratedFile(resourceFileName + ".Designer.cs", resourceFileDesignerContent)
+            };
         }
     }
 }
